4de48478fc27a7e86febf7e6b8ed4ea30ed3816a
StefanoGalandrini <stefe68@gmail.com>
2023-06-20 18:20:53 +0200

    Add composition class Category (Product has it)

---
+++ models/category.php
@@ -0,0 +1,22 @@
+<?php
+
+class Category
+{
+	// defines products for cats OR for dogs
+	public function __construct(
+		private string $type
+	) {
+		if ($type != "Cane" && $type != "Gatto") {
+			die("Si vendono solo articoli per cani o per gatti");
+		} else {
+			$this->type = $type;
+		}
+	}
+
+	// no need for checking errors here since once defined it's not going to change
+	// for the same reason, no setter is needed after it is constructed
+	public function getType(): string
+	{
+		return $this->type;
+	}
+}
